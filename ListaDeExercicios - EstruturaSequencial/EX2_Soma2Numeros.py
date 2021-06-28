e01feab13177f3900c7c25ca85f0b2e5a4ea8297
charlelito <charlelito@yahoo.com.br>
2021-06-28 12:40:05 -0300

    Update EX2_Soma2Numeros.py

--- ListaDeExercicios - EstruturaSequencial/EX2_Soma2Numeros.py
+++ ListaDeExercicios - EstruturaSequencial/EX2_Soma2Numeros.py
@@ -15,5 +15,5 @@ try:
     soma = num1+num2
     print(str(num1)+"+"+str(num2)+"="+str(soma))
 except ValueError:
-        print("Erro! Digite apenas números!")
+        print("Erro! Digite apenas números!!")
 
